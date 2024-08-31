f9234fff7412c8149a39a1338964669a90a3d745
sonya1045 <smikolaenko7@gmail.com>
2024-08-31 17:53:52 +0300

    update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from myapp.models import Teacher, Student, Subject, Class
+# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Class)
 # Register your models here.
